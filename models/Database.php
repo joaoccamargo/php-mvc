4b1c8d0c576232f31f5387e808ceed025b4fd1ea
João Camargo <jclaudio00_12@hotmail.com>
2023-10-04 21:17:20 -0300

    configurando pdo database

--- models/Database.php
+++ models/Database.php
@@ -1,2 +1,14 @@
 <?php
 
+class Database
+{
+    public function getConnection()
+    {
+        try {
+            $pdo = new PDO("mysql:dbname=dbapi;host=localhost", "root", "");
+            return $pdo;
+        } catch (PDOException $err) {
+            
+        }
+    }
+}
